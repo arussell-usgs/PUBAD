c19383e04eb456508ea6ea0014f6ad3dae31fd3c
arussell-usgs <arussell@usgs.gov>
2016-08-31 11:10:34 -0500

    added options for method of distance calculation, outlet or centroid, and generalized distance factor

--- R/indexNN.R
+++ R/indexNN.R
@@ -30,8 +30,27 @@
 #' \item{index}{The input \code{index.gages}.}
 #' \item{target}{The input \code{target.gages}.}
 #'@export
-indexNN <- function(index.gages,index.baschar,target.gages,target.baschar) {
-  # Function developed by William Farmer, 03 June 2015
+indexNN <- function(index.gages,index.baschar,target.gages,target.baschar,
+                    UTM=T, outlet=T, DAR.fact=0) {
+
+  # Modified by Tom Over and Amy Russell, June 2016
+
+  # Generalized from function indexNN developed by William Farmer, 03 June 2015
+
+  # Incorporates features of indexNN_centroid and indexNN_outlet
+  # created by Amy Russell, April 2016, and adds the option of including
+  # the DAR in the distance measure.
+  # New parameters:
+  # UTM -   if true (default), compute distance between gages (outlets) using UTM
+  #         locations; otherwise use Vincenty ellipsoid distance
+  # outlet - if true (default) and UTM is false, use Vincenty ellipsoid distance
+  #          between gages (outlets); if false and UTM is false,
+  #          use Vincenty ellipsoid distance between basins centroids
+  #          if UTM is true, outlet has no effect.
+  # DAR.fact - factor >=0 to weight abs(log10(DAR)) in computation of generalized distance,
+  #            (GD) which is computed as GD = Dist.Mat * (1 + DAR.fact*abs(log10(DAR))).
+  #            DAR.fact is dimensionless and defaults to zero in which case the DAR ratio
+  #            has no effect.
 
   Dist.Mat <- matrix(NA,ncol=length(target.gages),nrow=length(index.gages))
   DAR.Mat <- matrix(NA,ncol=length(target.gages),nrow=length(index.gages))
@@ -39,19 +58,42 @@ indexNN <- function(index.gages,index.baschar,target.gages,target.baschar) {
 
   for (i in 1:length(target.gages)) {
     # Compute distances between sites
-    Dist.Mat[,i] <- sqrt(
-      (index.baschar$LAT_GAGE_UTM-target.baschar$LAT_GAGE_UTM[i])^2 +
-        (index.baschar$LNG_GAGE_UTM-target.baschar$LNG_GAGE_UTM[i])^2
-    )
+    if (UTM) {
+      Dist.Mat[,i] <- sqrt(
+        (index.baschar$LAT_GAGE_UTM-target.baschar$LAT_GAGE_UTM[i])^2 +
+          (index.baschar$LNG_GAGE_UTM-target.baschar$LNG_GAGE_UTM[i])^2)
+    } else {
+
+      # Need index gages in matrix for Vincenty formula
+      #  Important that longitude is first column and latitude is second
+      if (outlet){
+
+        Index.LatLon <- data.frame(index.baschar$LNG_GAGE, index.baschar$LAT_GAGE)
+        Index.LatLon.Mat <- as.matrix(Index.LatLon)
+        Dist.Mat[,i] <- distVincentyEllipsoid(c(target.baschar$LNG_GAGE[i],
+                                                target.baschar$LAT_GAGE[i]),
+                                              Index.LatLon.Mat)
+      } else {
+
+        Index.LatLon <- data.frame(index.baschar$LONG_CENT, index.baschar$LAT_CENT)
+        Index.LatLon.Mat <- as.matrix(Index.LatLon)
+        Dist.Mat[,i] <- distVincentyEllipsoid(c(target.baschar$LONG_CENT[i],
+                                                target.baschar$LAT_CENT[i]),
+                                              Index.LatLon.Mat)
+      }
+    }
 
     # Calculate DAR between sites
     DAR.Mat[,i] <- target.baschar$DRAIN_SQKM[i]/index.baschar$DRAIN_SQKM
+
+    # Add DAR effect to Dist.Mat
+    Dist.Mat[,i] <- Dist.Mat[,i] * (1 + DAR.fact*abs(log10(DAR.Mat[,i])))
 
     # Rank index sites for each target
     sorted <- sort.int(Dist.Mat[,i],decreasing=FALSE,index.return=TRUE)
     Rank.Mat[,i] <- index.gages[sorted$ix]
   }
   result <- list(Dist=Dist.Mat,DAR=DAR.Mat,Rank=Rank.Mat,
-    index=index.gages,target=target.gages)
+                 index=index.gages,target=target.gages)
   return(result)
 }
